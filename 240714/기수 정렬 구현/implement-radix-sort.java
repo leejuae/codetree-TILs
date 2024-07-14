c6762506e842e00eb18e12a00fd4ffdaf4887141
이주애 <joojuae02@gachon.ac.kr>
2024-07-14 23:11:56 +0900

    Title: 기수 정렬 구현, Time: 436ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 240714/기수 정렬 구현/implement-radix-sort.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        radixSort(arr, n);
+
+        for(int i = 0; i < n; i++){
+            bw.write(arr[i] + " ");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void radixSort(int[] arr, int n) {
+        int max = getMax(arr, n);
+
+        // Apply counting sort to sort elements based on place value
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, n, exp);
+        }
+    }
+
+    public static void countingSort(int[] arr, int n, int exp) {
+        int[] output = new int[n]; // output array
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        // Store count of occurrences in count[]
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        // Change count[i] so that count[i] now contains actual
+        // position of this digit in output[]
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build the output array
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to current digit
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static int getMax(int[] arr, int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
